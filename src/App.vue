97753c84fde21d7f3fa9388c0402cc7b5528dfd6
taikyunn <taichi_141@me.com>
2021-11-06 23:04:55 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- htmlタグを含むデータの送る側 -->
+    <!-- htmlタグを含んで送るにはコンポーネントの中に書くと送れる。 -->
+    <!-- propsとの違いは、htmlテンプレートごと送信できる点にある -->
+    <LikeHeader headerText="Hello">
+      <h1>HTMLテンプレートを親コンポーネントから送信</h1>
+      <h2>{{ number }}</h2>
+
+      <!-- 名前付きスロット作成方法(送る側) -->
+      <!-- ①送りたい要素をtemplateタグで囲う -->
+      <!-- ②v-slot:名前という属性を与えてその中で送りたい特定のHTMLテンプレートを指定 -->
+      <template v-slot:title>
+        <h1>v-slotのタイトル</h1>
+      </template>
+      <h3>デフォルトスロット</h3>
+      <template v-slot:number>
+        <h2>{{ number }}</h2>
+      </template>
+
+      <!-- slotプロパティを使用して値を表示する -->
+      <!-- データの表示方法はv-slot:title='好きな名前'と定義することで'好きな名前'が子コンポーネントで送信した値に変わる。(今回はuserというオブジェクトになる) -->
+      <template v-slot:title2="slotProps">
+        <h1>子コンポーネントからのデータ</h1>
+        <h2>{{ slotProps.user.firstName }}</h2>
+        <h2>{{ slotProps.user.lastName  }}</h2>
+      </template>
+
+    <!-- 動的にタイトルを指定することができる -->
+    <template v-slot:[sample]>
+      <h1>動的にスロット名を指定</h1>
+    </template>
+
+    <!-- 省略記法 v-slot:は#に置き換えできる -->
+    <template #[sample2]>
+      <h1>v-slotの省略記法サンプル</h1>
+    </template>
+
+    </LikeHeader>
+    <!-- slotプロパティを使用して値を表示。かつコンポーネント内にtemplateタグがない場合(デフォルトtemplateのみしかない場合)はtemplateタグを用意しなくてもslotプロパティを使用できる。-->
+    <!-- またdefaultの表示も省略できる -->
+    <!-- LikeHeaderが今回は二つ存在しているので使えない為コメントアウトしている -->
+    <!-- <LikeHeader v-slot:default="slotProps">
+      <LikeHeader v-slot="slotProps">
+      <h1>2つ目のLikeHeader</h1>
+      {{ slotProps }}
+    </LikeHeader> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LikeHeader from './components/LikeHeader.vue'
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      number: 14,
+      sample: 'title3',
+      sample2: 'title4'
+    }
+  },
   components: {
-    HelloWorld
-  }
+    LikeHeader
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  /* h1 {
+    color: blue;
+  } */
 </style>
---
+++ src/components/LikeHeader.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <!-- htmlタグを含むデータの受け側 -->
+    <!-- slotタグを書くと親コンポーネントで定義したHTML要素を含むデータを受け取ることができる -->
+    <slot>
+      <!-- フォールバックコンテンツ -->
+      <!-- slotの中身が何もない場合のみ表示されるもの。今回は「デフォルトのタイトル」と表示される。 -->
+      <h1>デフォルトのタイトル</h1>
+    </slot>
+    <!-- slotは複数指定することもできる -->
+    <!-- 名前付きスロット作成方法(受け側) -->
+    <!-- 使用したいv-slotの親コンポーネントで指定した名前をname属性として指定することで完了 -->
+    <slot name="title"></slot>
+    <hr>
+    <slot name="number"></slot>
+    <hr>
+
+    <!-- 名前の付いていないslotはデフォルトスロットと呼ぶ -->
+    <!-- これはtemplateタグで囲われていない要素全てを上から順に表示してくれる -->
+    <!-- 実際はVue側が自動的にtemplateタグで、囲われていないものをv-slot="default"という名前で囲って子コンポーネントに持ってきてくれる。つまりv-slot:defaultが省略された形で書いても表示される仕様となっている -->
+    <slot></slot>
+
+    <!-- あまり使わないけど子コンポーネントから親コンポーネントにデータを渡すことができる -->
+    <!-- やり方：v-bind:送りたいデータ='好きな名前'で送信できる -->
+    <slot name="title2" v-bind:user='user'></slot>
+
+    <slot name="title3"></slot>
+    <slot name="title4"></slot>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  data() {
+    return {
+      user: {
+        firstName:'ドナルド',
+        lastName: 'トランプ',
+      }
+    }
+  }
+}
+</script>
+
+<!-- slotの中身データの変更はprops同様変更できない。ただしstyleに関しては子コンポーネント内でも変更させることができる。 -->
+<style scoped>
+  h1 {
+    color: red;
+  }
+</style>
